b04e59808e890879fd5530a301fd23b1671695ca
Orestis Rodriguez <orestis@orestisrodriguez.com>
2021-07-02 21:11:43 +0200

    chore: added test dependencies

---
+++ package-scripts.js
@@ -0,0 +1,9 @@
+module.exports = {
+  scripts: {
+    test: {
+      default: 'mocha test/**/*-test.js',
+      unit: 'mocha test/unit/*-test.js',
+      it: 'mocha test/it/*-test.js',
+    },
+  },
+}
